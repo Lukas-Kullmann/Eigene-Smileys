db87ef1a0e8f10240e07d8edc462fc894d9b0fd2
Satans-Kruemelmonster <lukaskullmann@web.de>
2012-03-19 18:50:30 +0100

    Neue Version - Version 3.0

--- release/update.user.js
+++ release/update.user.js
@@ -1,8 +1,14 @@
 // ==UserScript==
-// @name           own_smileys
-// @namespace      wop
-// @description    creates a smileybox below the standard-smiles
-// @include        http://forum.worldofplayers.*/forum/*
-// @author         Satans Krümelmonster
-// @version        2.5
+// @name            Eigene-Smileys
+// @namespace       wop
+// @description     creates a smileybox below the standard-smiles
+// @include         /^https{0,1}:\/\/forum\.worldofplayers\.[a-z]{2,4}\/forum\//
+// @author          Satans Krümelmonster
+// @version         3.0
+// @require         http://code.jquery.com/jquery-1.7.1.min.js
+// @require         https://raw.github.com/blueimp/JavaScript-Templates/master/tmpl.min.js
+// @require         https://github.com/douglascrockford/JSON-js/raw/master/json2.js
+// @downloadURL     https://github.com/Satans-Kruemelmonster/Eigene-Smileys/raw/master/release/eigene-smileys.user.js
+// @updateURL       https://github.com/Satans-Kruemelmonster/Eigene-Smileys/raw/master/release/update.user.js
+// @unwrap
 // ==/UserScript==
