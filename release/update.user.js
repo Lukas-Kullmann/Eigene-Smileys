69e96e877d2a5cc9fe28240a67c11b768524737c
Satans Krümelmonster <kruemel@assassins-creed.de>
2014-08-04 15:14:30 +0200

    Anpassen der Header- und Update der Release-Dateien

--- release/update.user.js
+++ release/update.user.js
@@ -4,10 +4,10 @@
 // @description     creates a smileybox below the standard-smiles
 // @include         /^https{0,1}:\/\/forum\.worldofplayers\.[a-z]{2,4}\/forum\//
 // @author          Satans Krümelmonster
-// @version         3.1
+// @version         3.2
 // @require         http://code.jquery.com/jquery-1.7.1.min.js
-// @require         https://raw.github.com/blueimp/JavaScript-Templates/6ad43cf40b1f7b0425bf9be41c1acdb64fd749a3/tmpl.min.js
-// @require         https://github.com/douglascrockford/JSON-js/raw/master/json2.js
+// @require         https://raw.githubusercontent.com/blueimp/JavaScript-Templates/b75e882ec0e353ce2d8bfdb2ea755060d2012368/js/tmpl.min.js
+// @require         https://raw.githubusercontent.com/douglascrockford/JSON-js/3d7767b6b1f3da363c625ff54e63bbf20e9e83ac/json2.js
 // @downloadURL     https://github.com/Satans-Kruemelmonster/Eigene-Smileys/raw/master/release/eigene-smileys.user.js
 // @updateURL       https://github.com/Satans-Kruemelmonster/Eigene-Smileys/raw/master/release/update.user.js
 // @unwrap
